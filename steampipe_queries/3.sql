9f108e212cb57472d3dbf147bb41a4127fef1d35
vijayvat <vpgvijay@yahoo.com>
2024-12-16 18:43:18 +0530

    Update 3.sql

--- steampipe_queries/3.sql
+++ steampipe_queries/3.sql
@@ -1,11 +1,8 @@
 select 
-  case 
-    when count(*) = 0 then 
-      (select 1/0)
-    else 
-      count(*)
-  end as non_compliant_count
+  task_arn,
+  region,
+  tags
 from aws_ecs_task 
 where launch_type = 'FARGATE'
   and region = 'us-west-2'
-  and not tags ?& array['Environment', 'Owner', 'CostCenter'];
+  and tags ?& array['Environment', 'Owner', 'CostCenter'];
