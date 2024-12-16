e77b389c4c41a07fdb860fcdc49e29be7e5df8b8
vijayvat <vpgvijay@yahoo.com>
2024-12-16 18:47:44 +0530

    Update 3.sql

--- steampipe_queries/3.sql
+++ steampipe_queries/3.sql
@@ -3,6 +3,5 @@ select
   region,
   tags
 from aws_ecs_task 
-where launch_type = 'FARGATE'
-  and region = 'us-west-2'
+where region = 'us-west-2'
   and tags ?& array['Environment', 'Owner', 'CostCenter'];
