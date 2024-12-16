0eedfbcd1d61d66f1faa8edf7074fad584d4c3b3
vijayvat <vpgvijay@yahoo.com>
2024-12-16 18:05:40 +0530

    Update 3.sql

--- steampipe_queries/3.sql
+++ steampipe_queries/3.sql
@@ -1,6 +1,11 @@
 select 
   task_arn,
   region,
-  launch_type,
-  last_status
-from aws_ecs_task;
+  last_status,
+  desired_status,
+  tags,
+  created_at
+from aws_ecs_task 
+where launch_type = 'FARGATE'
+  and region = 'us-west-2'
+  and not tags ?& array['Environment', 'Owner', 'CostCenter'];
