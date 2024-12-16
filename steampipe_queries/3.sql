00eda2ed7b1571b86f8facf2da1f353afeadf02d
vijayvat <vpgvijay@yahoo.com>
2024-12-16 18:08:52 +0530

    Update 3.sql

--- steampipe_queries/3.sql
+++ steampipe_queries/3.sql
@@ -1,10 +1,11 @@
 select 
-  task_arn,
-  region,
-  last_status,
-  desired_status,
-  tags,
-  created_at
+  case 
+    when count(*) = 0 then 
+      -- All tasks have required tags, so make query fail
+      (select 1/0)  -- This forces error/non-zero exit
+    else 
+      count(*) -- Return count of non-compliant tasks
+  end as non_compliant_count
 from aws_ecs_task 
 where launch_type = 'FARGATE'
   and region = 'us-west-2'
